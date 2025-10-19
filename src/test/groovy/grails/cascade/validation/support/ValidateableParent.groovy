87d48a4fdcbc3e783d43108c6cd6437f4898b136
Søren Berg Glasius <soeren@glasius.dk>
2025-10-19 21:50:01 +0200

    Implemented cascades with an evaluated closure
    Fixes #9

--- src/test/groovy/grails/cascade/validation/support/ValidateableParent.groovy
+++ src/test/groovy/grails/cascade/validation/support/ValidateableParent.groovy
@@ -6,9 +6,13 @@ import grails.validation.Validateable
 class ValidateableParent implements Validateable {
 
     ValidateableProperty property
-
+    ValidateableProperty anotherProperty
+    
+    boolean shouldCascade() { true }
+    
     static constraints = {
         property cascaded: true
+        anotherProperty cascade: { value, object -> object.shouldCascade() }
     }
 }
 // end::code[]
